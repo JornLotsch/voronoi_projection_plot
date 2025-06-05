d15ed738583baa18a5a8ee84c016ff2ad756e8d2
Jörn Lötsch <jloetsch@rz.uni-frankfurt.de>
2025-06-05 12:50:32 +0200

    MetaboAnalyst style plot added for comparison

--- covid_metaboanalyst_example.R
+++ covid_metaboanalyst_example.R
@@ -1,4 +1,6 @@
 ############### Read the COVID data ##############################
+library(stringr)
+
 covid_metaboanalyst_pca_score <- read.csv("covid_metabolomics_pca_score.csv", row.names = 1)
 covid_metaboanalyst_pca_score$covid <- ifelse(str_detect(rownames(covid_metaboanalyst_pca_score), "Pt"), 1,
                 ifelse(str_detect(rownames(covid_metaboanalyst_pca_score), "HC"), 0, NA))
@@ -40,19 +42,64 @@ covid_voronoi_ellipse_plot <- covid_tests_data_complete_plot$voronoi_plot_plus_e
     subtitle = "Voronoi tesselation and confidence ellipses for prior classes"
   )
 
+# Additional contour plot because of the MetaboAnalyst default
+
+# Helper function to get plot limits
+get_plot_limits <- function(plot) {
+  gb <- ggplot2::ggplot_build(plot)
+  list(
+    xmin = gb$layout$panel_params[[1]]$x.range[1],
+    xmax = gb$layout$panel_params[[1]]$x.range[2],
+    ymin = gb$layout$panel_params[[1]]$y.range[1],
+    ymax = gb$layout$panel_params[[1]]$y.range[2]
+  )
+}
+
+
+covid_voronoi_contur_plot <- 
+  ggplot(data = covid_metaboanalyst_pca_score, aes(x = PC1, y = PC2, color = as.factor(covid), fill = as.factor(covid))) +
+  geom_point() +
+  stat_density_2d(show.legend = FALSE) + 
+  theme_light() +
+  ggthemes::scale_color_colorblind() +
+  ggthemes::scale_fill_colorblind()+
+  labs(
+    title = paste0("COVID data: ", projection_method, " projection"),
+    subtitle = "Contour plot analogous to the MetaboAnalyst default"
+  ) +
+  xlim(unlist(get_plot_limits(covid_ellipse_plot))[1:2]) +
+  ylim(unlist(get_plot_limits(covid_ellipse_plot))[3:4]) 
+  
 # Combine plots
-covid_combined_visualization_6 <- cowplot::plot_grid(
+covid_combined_visualization_3 <- cowplot::plot_grid(
   covid_ellipse_plot,
   covid_voronoi_plot,
   covid_voronoi_ellipse_plot,
   labels = "AUTO",
   nrow = 1
 )
-print(covid_combined_visualization_6)
-
+print(covid_combined_visualization_3)
 ggsave(
-  filename = "covid_combined_visualization_6.svg",
-  plot = covid_combined_visualization_6,
+  filename = "covid_combined_visualization_3.svg",
+  plot = covid_combined_visualization_3,
   width = 27,
   height = 9
+)
+
+# Combine plots 4
+covid_combined_visualization_4 <- cowplot::plot_grid(
+  covid_ellipse_plot,
+  covid_voronoi_plot,
+  covid_voronoi_ellipse_plot,
+  covid_voronoi_contur_plot,
+  labels = "AUTO",
+  nrow = 2
+)
+print(covid_combined_visualization_4)
+
+ggsave(
+  filename = "covid_combined_visualization_4.svg",
+  plot = covid_combined_visualization_4,
+  width = 16,
+  height = 16
 )
